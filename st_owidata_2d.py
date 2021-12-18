d955ae21b72ba1939d29d53618ba8dc9008203de
gatan1962 <95849947+gatan1962@users.noreply.github.com>
2021-12-18 15:46:51 +0800

    Remove unwanted comments & test codes.

--- st_owidata_2d.py
+++ st_owidata_2d.py
@@ -110,28 +110,21 @@ def check_raw_data(input_data, header):
 # program initialization
 st.title('Covid-19 Trend Chart')
 st.text('Data source:- https://covid.ourworldindata.org/data/owid-covid-data.csv')
-# user input country name
-#st.write('## Enter country:')
 country_name = st.text_input('Enter country:')
 st.write('Selected country is:', country_name)
 
 # Download covid data from owid url
-#data_load_status = st.write("Downloading covid data from owid in progress...")
 data, country_list = load_data(country_name) #('China')
 
 if country_name not in country_list:
     st.warning('Oops...! Country not entered or not valid, please re-enter!')
 
 # use a button to toggle country names
-#st.write(country_list)
 if st.checkbox('Show all country names'):
     check_raw_data(country_list, 'List of sorted country names:')
     
 if st.checkbox('Raw data'):
     check_raw_data(data, 'Raw data of selected country')  
-#st.write(data.shape)
-#st.write(data)
-
 
 ######################################################
 ######## 1. Define data of interest       ############
@@ -157,11 +150,7 @@ try:
 
     fig, ax = plt.subplots(2, figsize=(8,6))
     ax[0].scatter(x1, y1)
-    ax[1].scatter(x1, y2)
-    #fig = plot_chart(x1, y1, country_name, ylabel, ticks=freq)
-    
-    ###---
-    #ax.scatter(x_lst,y_lst)
+    ax[1].scatter(x1, y2)   
     ticks, country = freq, country_name
 
     ax[0].set_title(country)
@@ -170,8 +159,6 @@ try:
     ax[0].set_xticks(x1[::ticks])
     ax[0].set_xticklabels([]) #x1[::ticks], rotation=90, fontsize=8)
     ax[0].grid(linestyle='--')
-    #plt.yticks(fontsize=30)
-    #ax[0].set_yticklabels(y1[::100],fontsize=20)
     
     ax[1].set_xlabel('Date')
     ax[1].set_ylabel(y_label[1])
@@ -179,15 +166,10 @@ try:
     ax[1].set_xticklabels(x1[::ticks], rotation=90, fontsize=8)    
     ax[1].grid(linestyle='--')
     
-    #plt.xticks(x1[::ticks], rotation=90, fontsize=8)
-    #plt.yticks(fontsize=10)
-    #plt.grid(linestyle='--')
-    
-    ###---
     fig.tight_layout()
     st.pyplot(fig)
 
 except:
-    st.warning('Oops...! Please check if country is entered correctly.')
+    st.warning('Oops...! Please check if the country name is entered correctly.')
     
 
